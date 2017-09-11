5292734f2dbefa374ce478a6132c5dd4286106f1
sekharbandla <rajasekhar.bandla@reancloud.com>
2017-09-11 12:19:58 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: WCFC
-# Author:: pavankumar.amballa@reancloud.com
+# Author:: rajsekhar.bandla@reancloud.com
 
 # This is a Chef attributes file. It can be used to specify default and override
 # attributes to be applied to nodes that run this cookbook.
